0b1e205e75715b6b39723087d0c7bd508d35e231
Tom Kapanka <spanky@kapanka.com>
2014-01-08 21:37:38 -0800

    Remove all niceties

--- rubberband.sh
+++ rubberband.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-echo
-echo -e "\e[1mWelcome to RubberBand!\e[0m"
-echo "A friendly script to diagnose your ElasticSearch cluster"
-
-echo -n "Ready to go, are you? y/N "
-read -n 1 ready
-echo
-if [ "$ready" != "y" ]
-then
-    echo
-    echo -e "\e[10;35mABORT!\e[0m"
-    echo
-    exit 1
-fi
-
 commands=('_cluster/health?pretty' '_mapping?pretty' '_settings?pretty' '_cluster/state?pretty'  '_cluster/settings?pretty' '_stats?all&pretty' '_nodes?all&pretty' '_nodes/stats?all&pretty')
 
 echo "{"
